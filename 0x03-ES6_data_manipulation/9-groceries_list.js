b33ec38e5240b9c508cb6f5ec5657a3c0aa17b93
leworkalush <81740368+leworkalush@users.noreply.github.com>
2023-10-03 12:25:55 +0300

    Create 9-groceries_list.js

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,10 @@
+export default function groceriesList() {
+  /* eslint-disable */
+  const map = new Map();
+  map.set('Apples', 10);
+  map.set('Tomatoes', 10);
+  map.set('Pasta', 1);
+  map.set('Rice', 1);
+  map.set('Banana', 5);
+  return map;
+};
