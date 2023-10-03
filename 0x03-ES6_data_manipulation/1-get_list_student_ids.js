665b6b39fa84f219f459cd357cdf769f10bb3a13
leworkalush <81740368+leworkalush@users.noreply.github.com>
2023-10-03 12:20:48 +0300

    Create 1-get_list_student_ids.js

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,9 @@
+export default function getListStudentIds(listStudents) {
+  /* eslint-disable */
+  let listStudentIds = [];
+  if (!(listStudents instanceof Array)) {
+    return listStudentIds;
+  }
+  listStudentIds = listStudents.map(student => student.id);
+  return listStudentIds;
+}
